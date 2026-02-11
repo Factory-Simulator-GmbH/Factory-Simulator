62dbebe845877fe53b5cacc7668d5108165307cb
Jan Lehmann <Jan_Lehmann@sluz.ch>
2026-02-11 13:02:47 +0100

    integrated interactjs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import {Component, signal} from '@angular/core';
+import {Component, signal, AfterViewInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import interact from 'interactjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  standalone: true
+  standalone: true,
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('factory-simulator');
+
+  ngAfterViewInit() {
+    interact('.draggable').draggable({
+      listeners: {
+        move(event) {
+          const target = event.target as HTMLElement;
+          const x = (parseFloat(target.getAttribute('data-x') || '0') || 0) + event.dx;
+          const y = (parseFloat(target.getAttribute('data-y') || '0') || 0) + event.dy;
+
+          target.style.transform = `translate(${x}px, ${y}px)`;
+          target.setAttribute('data-x', String(x));
+          target.setAttribute('data-y', String(y));
+        },
+      },
+    });
+  }
 }
