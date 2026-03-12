90451f46e560633e560e0eba217022ef35cc23b5
Jan Lehmann <Jan_Lehmann@sluz.ch>
2026-03-12 11:26:18 +0100

    adjusted tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,23 @@
 // Auto generated file to test the app
 import {TestBed} from '@angular/core/testing';
-import {App} from './apps';
+import {AppComponent} from './app.component';
 
 // Test for the Components of the App
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Factory Simulator');
