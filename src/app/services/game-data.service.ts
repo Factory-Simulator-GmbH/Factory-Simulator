ac2e54d9531dad8ac8d5737440b1dd6498b79357
Sarah <sarahchristen74@gmail.com>
2026-06-23 11:16:57 +0200

    filter von type zu filter_category changed

--- src/app/services/game-data.service.ts
+++ src/app/services/game-data.service.ts
@@ -20,6 +20,7 @@ export class GameDataService {
     return (data ?? []).map(row => ({
       id: row['id'],
       type: row['type'],
+      filter_category: row['filter_category'] ?? undefined,
       label: row['label'],
       size: row['size'],
       helpText: row['help_text'],
